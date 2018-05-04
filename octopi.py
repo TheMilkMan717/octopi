81a5861818ece25f6bbc1fbf3e10bbdf6eebdbc7
TheMilkMan717 <ssakol97@terpmail.umd.edu>
2018-05-04 11:21:07 -0700

    working on remote turn off and turn on

--- octopi.py
+++ octopi.py
@@ -6,8 +6,29 @@ import argparse
 import sys
 import os
 
+banner = "EASTER BUNNY'S FLUFFY WHITE COCK"
+start_up_banner = "CAESAR SALAD COCK"
+flushed = False
 
 def spoof_scan(packet):
+    global flushed  
+    if flushed:
+        pkt = IP(packet.get_payload())
+        print "HERE"
+        # pkt.show()
+        if pkt.haslayer(TCP) and pkt.haslayer(Raw):
+            raw_bytes = pkt["Raw"].load.decode("utf-8")
+            if start_up_banner in raw_bytes:
+                flushed = False
+                # os.system("iptables -A INPUT -j NFQUEUE --queue-num 1")
+                print "turned back on"
+                packet.accept()
+                return
+
+        else:
+            packet.accept()
+            return
+
     try:
         pkt = IP(packet.get_payload())
 
@@ -31,15 +52,30 @@ def spoof_scan(packet):
             print "Scan:\t%d" % ret_pkt["TCP"].sport
             send(ret_pkt, verbose=False)
 
+        # kill switch
+        elif tcp_flag == "PA":
+            if not pkt.haslayer(Raw):
+                packet.accept()
+                return
+            else:
+                raw_bytes = pkt["Raw"].load.decode("utf-8")
+                if banner in raw_bytes:
+                    packet.drop()
+                    flushed = True
+                    print "turned it off"
+                    # os.system("iptables -F")
+
+
         else:
             packet.accept()
         
 
     except Exception as e:
-        print e
+        # print e
         print "ERROR"
 
 if __name__ == "__main__":
+    global nfqueue
     parser = argparse.ArgumentParser()
     # parser.add_argument("", type=str)
     args = parser.parse_args()
@@ -56,7 +92,9 @@ if __name__ == "__main__":
         print "CREATED NFQUEUE..."
         nfqueue.bind(1, spoof_scan)
         nfqueue.run()
+
     except KeyboardInterrupt:
         nfqueue.unbind()
+        print "Ending Octopi"
         os.system("iptables -F")
         sys.exit(0)
